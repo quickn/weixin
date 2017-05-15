aef22da23a183031a56022857d52da030ade542e
liuyun <260418992@qq.com>
2017-05-16 01:08:26 +0800

    修改客服消息接口

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/WxMpKefuService.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/WxMpKefuService.java
@@ -25,7 +25,7 @@ public interface WxMpKefuService {
    * 接口url格式：https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=ACCESS_TOKEN
    * </pre>
    */
-  boolean sendKefuMessage(WxMpKefuMessage message) throws WxErrorException;
+  boolean sendKefuMessage(WxMpKefuMessage message,String... authorizerAppid) throws WxErrorException;
 
   //*******************客服管理接口***********************//
 
@@ -36,7 +36,7 @@ public interface WxMpKefuService {
    * 接口url格式：https://api.weixin.qq.com/cgi-bin/customservice/getkflist?access_token=ACCESS_TOKEN
    * </pre>
    */
-  WxMpKfList kfList() throws WxErrorException;
+  WxMpKfList kfList(String... authorizerAppid) throws WxErrorException;
 
   /**
    * <pre>
@@ -45,7 +45,7 @@ public interface WxMpKefuService {
    * 接口url格式：https://api.weixin.qq.com/cgi-bin/customservice/getonlinekflist?access_token=ACCESS_TOKEN
    * </pre>
    */
-  WxMpKfOnlineList kfOnlineList() throws WxErrorException;
+  WxMpKfOnlineList kfOnlineList(String... authorizerAppid) throws WxErrorException;
 
   /**
    * <pre>
@@ -54,7 +54,7 @@ public interface WxMpKefuService {
    * 接口url格式：https://api.weixin.qq.com/customservice/kfaccount/add?access_token=ACCESS_TOKEN
    * </pre>
    */
-  boolean kfAccountAdd(WxMpKfAccountRequest request) throws WxErrorException;
+  boolean kfAccountAdd(WxMpKfAccountRequest request,String... authorizerAppid) throws WxErrorException;
 
   /**
    * <pre>
@@ -63,7 +63,7 @@ public interface WxMpKefuService {
    * 接口url格式：https://api.weixin.qq.com/customservice/kfaccount/update?access_token=ACCESS_TOKEN
    * </pre>
    */
-  boolean kfAccountUpdate(WxMpKfAccountRequest request) throws WxErrorException;
+  boolean kfAccountUpdate(WxMpKfAccountRequest request,String... authorizerAppid) throws WxErrorException;
 
   /**
    * <pre>
@@ -72,7 +72,7 @@ public interface WxMpKefuService {
    * 接口url格式：https://api.weixin.qq.com/customservice/kfaccount/inviteworker?access_token=ACCESS_TOKEN
    * </pre>
    */
-  boolean kfAccountInviteWorker(WxMpKfAccountRequest request) throws WxErrorException;
+  boolean kfAccountInviteWorker(WxMpKfAccountRequest request,String... authorizerAppid) throws WxErrorException;
 
   /**
    * <pre>
@@ -81,7 +81,7 @@ public interface WxMpKefuService {
    * 接口url格式：http://api.weixin.qq.com/customservice/kfaccount/uploadheadimg?access_token=ACCESS_TOKEN&kf_account=KFACCOUNT
    * </pre>
    */
-  boolean kfAccountUploadHeadImg(String kfAccount, File imgFile)
+  boolean kfAccountUploadHeadImg(String kfAccount, File imgFile,String... authorizerAppid)
           throws WxErrorException;
 
   /**
@@ -91,7 +91,7 @@ public interface WxMpKefuService {
    * 接口url格式：https://api.weixin.qq.com/customservice/kfaccount/del?access_token=ACCESS_TOKEN&kf_account=KFACCOUNT
    * </pre>
    */
-  boolean kfAccountDel(String kfAccount) throws WxErrorException;
+  boolean kfAccountDel(String kfAccount,String... authorizerAppid) throws WxErrorException;
 
   //*******************客服会话控制接口***********************//
 
@@ -103,7 +103,7 @@ public interface WxMpKefuService {
    * 接口url格式： https://api.weixin.qq.com/customservice/kfsession/create?access_token=ACCESS_TOKEN
    * </pre>
    */
-  boolean kfSessionCreate(String openid, String kfAccount) throws WxErrorException;
+  boolean kfSessionCreate(String openid, String kfAccount,String... authorizerAppid) throws WxErrorException;
 
   /**
    * <pre>
@@ -113,7 +113,7 @@ public interface WxMpKefuService {
    * 接口url格式： https://api.weixin.qq.com/customservice/kfsession/close?access_token=ACCESS_TOKEN
    * </pre>
    */
-  boolean kfSessionClose(String openid, String kfAccount) throws WxErrorException;
+  boolean kfSessionClose(String openid, String kfAccount,String... authorizerAppid) throws WxErrorException;
 
   /**
    * <pre>
@@ -123,7 +123,7 @@ public interface WxMpKefuService {
    * 接口url格式： https://api.weixin.qq.com/customservice/kfsession/getsession?access_token=ACCESS_TOKEN&openid=OPENID
    * </pre>
    */
-  WxMpKfSessionGetResult kfSessionGet(String openid) throws WxErrorException;
+  WxMpKfSessionGetResult kfSessionGet(String openid,String... authorizerAppid) throws WxErrorException;
 
   /**
    * <pre>
@@ -133,7 +133,7 @@ public interface WxMpKefuService {
    * 接口url格式： https://api.weixin.qq.com/customservice/kfsession/getsessionlist?access_token=ACCESS_TOKEN&kf_account=KFACCOUNT
    * </pre>
    */
-  WxMpKfSessionList kfSessionList(String kfAccount) throws WxErrorException;
+  WxMpKfSessionList kfSessionList(String kfAccount,String... authorizerAppid) throws WxErrorException;
 
   /**
    * <pre>
@@ -143,7 +143,7 @@ public interface WxMpKefuService {
    * 接口url格式： https://api.weixin.qq.com/customservice/kfsession/getwaitcase?access_token=ACCESS_TOKEN
    * </pre>
    */
-  WxMpKfSessionWaitCaseList kfSessionGetWaitCase() throws WxErrorException;
+  WxMpKfSessionWaitCaseList kfSessionGetWaitCase(String... authorizerAppid) throws WxErrorException;
 
   //*******************获取聊天记录的接口***********************//
 
@@ -162,7 +162,7 @@ public interface WxMpKefuService {
    * @return 聊天记录对象
    * @throws WxErrorException
    */
-  WxMpKfMsgList kfMsgList(Date startTime, Date endTime, Long msgId, Integer number) throws WxErrorException;
+  WxMpKfMsgList kfMsgList(Date startTime, Date endTime, Long msgId, Integer number,String... authorizerAppid) throws WxErrorException;
 
   /**
    * <pre>
@@ -177,6 +177,6 @@ public interface WxMpKefuService {
    * @return 聊天记录对象
    * @throws WxErrorException
    */
-  WxMpKfMsgList kfMsgList(Date startTime, Date endTime) throws WxErrorException;
+  WxMpKfMsgList kfMsgList(Date startTime, Date endTime,String... authorizerAppid) throws WxErrorException;
 
 }
--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/impl/WxMpKefuServiceImpl.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/impl/WxMpKefuServiceImpl.java
@@ -1,28 +1,21 @@
 package me.chanjar.weixin.mp.api.impl;
 
-import java.io.File;
-import java.util.Date;
-
-import me.chanjar.weixin.mp.bean.kefu.WxMpKefuMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.gson.JsonObject;
-
 import me.chanjar.weixin.common.bean.result.WxError;
 import me.chanjar.weixin.common.bean.result.WxMediaUploadResult;
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.common.util.http.MediaUploadRequestExecutor;
 import me.chanjar.weixin.mp.api.WxMpKefuService;
 import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.kefu.WxMpKefuMessage;
 import me.chanjar.weixin.mp.bean.kefu.request.WxMpKfAccountRequest;
 import me.chanjar.weixin.mp.bean.kefu.request.WxMpKfSessionRequest;
-import me.chanjar.weixin.mp.bean.kefu.result.WxMpKfList;
-import me.chanjar.weixin.mp.bean.kefu.result.WxMpKfMsgList;
-import me.chanjar.weixin.mp.bean.kefu.result.WxMpKfOnlineList;
-import me.chanjar.weixin.mp.bean.kefu.result.WxMpKfSessionGetResult;
-import me.chanjar.weixin.mp.bean.kefu.result.WxMpKfSessionList;
-import me.chanjar.weixin.mp.bean.kefu.result.WxMpKfSessionWaitCaseList;
+import me.chanjar.weixin.mp.bean.kefu.result.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Date;
 
 /**
  *
@@ -41,110 +34,110 @@ public class WxMpKefuServiceImpl implements WxMpKefuService {
   }
 
   @Override
-  public boolean sendKefuMessage(WxMpKefuMessage message)
+  public boolean sendKefuMessage(WxMpKefuMessage message,String... authorizerAppid )
       throws WxErrorException {
     String url = "https://api.weixin.qq.com/cgi-bin/message/custom/send";
-    String responseContent = this.wxMpService.post(url, message.toJson());
+    String responseContent = this.wxMpService.post(url, message.toJson(),authorizerAppid);
     return responseContent != null;
   }
 
   @Override
-  public WxMpKfList kfList() throws WxErrorException {
+  public WxMpKfList kfList(String... authorizerAppid) throws WxErrorException {
     String url = API_URL_PREFIX_WITH_CGI_BIN + "/getkflist";
-    String responseContent = this.wxMpService.get(url, null);
+    String responseContent = this.wxMpService.get(url, null,authorizerAppid);
     return WxMpKfList.fromJson(responseContent);
   }
 
   @Override
-  public WxMpKfOnlineList kfOnlineList() throws WxErrorException {
+  public WxMpKfOnlineList kfOnlineList(String... authorizerAppid) throws WxErrorException {
     String url = API_URL_PREFIX_WITH_CGI_BIN + "/getonlinekflist";
-    String responseContent = this.wxMpService.get(url, null);
+    String responseContent = this.wxMpService.get(url, null,authorizerAppid);
     return WxMpKfOnlineList.fromJson(responseContent);
   }
 
   @Override
-  public boolean kfAccountAdd(WxMpKfAccountRequest request)
+  public boolean kfAccountAdd(WxMpKfAccountRequest request,String... authorizerAppid)
       throws WxErrorException {
     String url = API_URL_PREFIX + "/kfaccount/add";
-    String responseContent = this.wxMpService.post(url, request.toJson());
+    String responseContent = this.wxMpService.post(url, request.toJson(),authorizerAppid);
     return responseContent != null;
   }
 
   @Override
-  public boolean kfAccountUpdate(WxMpKfAccountRequest request)
+  public boolean kfAccountUpdate(WxMpKfAccountRequest request,String... authorizerAppid)
       throws WxErrorException {
     String url = API_URL_PREFIX + "/kfaccount/update";
-    String responseContent = this.wxMpService.post(url, request.toJson());
+    String responseContent = this.wxMpService.post(url, request.toJson(),authorizerAppid);
     return responseContent != null;
   }
 
   @Override
-  public boolean kfAccountInviteWorker(WxMpKfAccountRequest request) throws WxErrorException {
+  public boolean kfAccountInviteWorker(WxMpKfAccountRequest request,String... authorizerAppid) throws WxErrorException {
     String url = API_URL_PREFIX + "/kfaccount/inviteworker";
-    String responseContent = this.wxMpService.post(url, request.toJson());
+    String responseContent = this.wxMpService.post(url, request.toJson(),authorizerAppid);
     return responseContent != null;
   }
 
   @Override
-  public boolean kfAccountUploadHeadImg(String kfAccount, File imgFile)
+  public boolean kfAccountUploadHeadImg(String kfAccount, File imgFile,String... authorizerAppid)
       throws WxErrorException {
     String url = API_URL_PREFIX + "/kfaccount/uploadheadimg?kf_account=" + kfAccount;
     WxMediaUploadResult responseContent = this.wxMpService
-        .execute(new MediaUploadRequestExecutor(), url, imgFile);
+        .execute(new MediaUploadRequestExecutor(), url, imgFile,authorizerAppid);
     return responseContent != null;
   }
 
   @Override
-  public boolean kfAccountDel(String kfAccount) throws WxErrorException {
+  public boolean kfAccountDel(String kfAccount,String... authorizerAppid) throws WxErrorException {
     String url = API_URL_PREFIX + "/kfaccount/del?kf_account=" + kfAccount;
-    String responseContent = this.wxMpService.get(url, null);
+    String responseContent = this.wxMpService.get(url, null,authorizerAppid);
     return responseContent != null;
   }
 
   @Override
-  public boolean kfSessionCreate(String openid, String kfAccount)
+  public boolean kfSessionCreate(String openid, String kfAccount,String... authorizerAppid)
       throws WxErrorException {
     WxMpKfSessionRequest request = new WxMpKfSessionRequest(kfAccount, openid);
     String url = API_URL_PREFIX + "/kfsession/create";
-    String responseContent = this.wxMpService.post(url, request.toJson());
+    String responseContent = this.wxMpService.post(url, request.toJson(),authorizerAppid);
     return responseContent != null;
   }
 
   @Override
-  public boolean kfSessionClose(String openid, String kfAccount)
+  public boolean kfSessionClose(String openid, String kfAccount,String... authorizerAppid)
       throws WxErrorException {
     WxMpKfSessionRequest request = new WxMpKfSessionRequest(kfAccount, openid);
     String url = API_URL_PREFIX + "/kfsession/close";
-    String responseContent = this.wxMpService.post(url, request.toJson());
+    String responseContent = this.wxMpService.post(url, request.toJson(),authorizerAppid);
     return responseContent != null;
   }
 
   @Override
-  public WxMpKfSessionGetResult kfSessionGet(String openid)
+  public WxMpKfSessionGetResult kfSessionGet(String openid,String... authorizerAppid)
       throws WxErrorException {
     String url = API_URL_PREFIX + "/kfsession/getsession?openid=" + openid;
-    String responseContent = this.wxMpService.get(url, null);
+    String responseContent = this.wxMpService.get(url, null,authorizerAppid);
     return WxMpKfSessionGetResult.fromJson(responseContent);
   }
 
   @Override
-  public WxMpKfSessionList kfSessionList(String kfAccount)
+  public WxMpKfSessionList kfSessionList(String kfAccount,String... authorizerAppid)
       throws WxErrorException {
     String url = API_URL_PREFIX + "/kfsession/getsessionlist?kf_account=" + kfAccount;
-    String responseContent = this.wxMpService.get(url, null);
+    String responseContent = this.wxMpService.get(url, null,authorizerAppid);
     return WxMpKfSessionList.fromJson(responseContent);
   }
 
   @Override
-  public WxMpKfSessionWaitCaseList kfSessionGetWaitCase()
+  public WxMpKfSessionWaitCaseList kfSessionGetWaitCase(String... authorizerAppid)
       throws WxErrorException {
     String url = API_URL_PREFIX + "/kfsession/getwaitcase";
-    String responseContent = this.wxMpService.get(url, null);
+    String responseContent = this.wxMpService.get(url, null,authorizerAppid);
     return WxMpKfSessionWaitCaseList.fromJson(responseContent);
   }
 
   @Override
-  public WxMpKfMsgList kfMsgList(Date startTime, Date endTime, Long msgId, Integer number) throws WxErrorException {
+  public WxMpKfMsgList kfMsgList(Date startTime, Date endTime, Long msgId, Integer number,String... authorizerAppid) throws WxErrorException {
     if(number > 10000){
       throw new WxErrorException(WxError.newBuilder().setErrorMsg("非法参数请求，每次最多查询10000条记录！").build());
     }
@@ -161,13 +154,13 @@ public class WxMpKefuServiceImpl implements WxMpKefuService {
     param.addProperty("msgid", msgId); //msgid	消息id顺序从小到大，从1开始
     param.addProperty("number", number); //number	每次获取条数，最多10000条
 
-    String responseContent = this.wxMpService.post(url, param.toString());
+    String responseContent = this.wxMpService.post(url, param.toString(),authorizerAppid);
 
     return WxMpKfMsgList.fromJson(responseContent);
   }
 
   @Override
-  public WxMpKfMsgList kfMsgList(Date startTime, Date endTime) throws WxErrorException {
+  public WxMpKfMsgList kfMsgList(Date startTime, Date endTime,String... authorizerAppid) throws WxErrorException {
     int number = 10000;
     WxMpKfMsgList result =  this.kfMsgList(startTime,endTime, 1L, number);
 
